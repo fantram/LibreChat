0fdb53d7d88b8574b7d9d7165f08a5b6af15fc5c
Franz Zetzsch <franzzetzsch@MacBook-Pro-von-Franz.local>
2025-07-05 21:12:11 +0200

    Initial Commit CI Changes to test pipeline

--- client/src/components/Auth/LoginForm.tsx
+++ client/src/components/Auth/LoginForm.tsx
@@ -165,8 +165,8 @@ const LoginForm: React.FC<TLoginFormProps> = ({ onSubmit, startupConfig, error, 
             data-testid="login-button"
             type="submit"
             className="
-            w-full rounded-2xl bg-green-600 px-4 py-3 text-sm font-medium text-white
-            transition-colors hover:bg-green-700 dark:bg-green-600 dark:hover:bg-green-700
+            w-full rounded-2xl bg-primary px-4 py-3 text-sm font-medium text-white
+            transition-colors hover:bg-secondary
           "
           >
             {localize('com_auth_continue')}
